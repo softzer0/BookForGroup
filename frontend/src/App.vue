b299a0b9b08930736e8e4e74db16455f6ba4fc21
softzer0 <mihailosoft@gmail.com>
2020-04-11 15:52:04 +0200

    Vuetify started to work

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="app">
+  <v-app>
     <Header/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </v-app>
@@ -19,12 +19,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
