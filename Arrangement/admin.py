b3a3066a959f88ac431086a133fc334e5bd1e1d2
NemanjaSt97 <stojanovic.nemanja71@gmail.com>
2020-04-15 09:03:59 +0200

    Customizing the Admin

--- Arrangement/admin.py
+++ Arrangement/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Arrangement
 
-# Register your models here.
+
+class ArrangementAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'user', 'numberOfPeople', 'price', 'discount')
+
+
+admin.site.register(Arrangement, ArrangementAdmin)
--- Hotel/admin.py
+++ Hotel/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Hotel, Room
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'numberOfPeople', 'time')
+
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'webSite')
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Hotel, HotelAdmin)
