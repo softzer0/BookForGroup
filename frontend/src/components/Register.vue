f705671fcd3e39820c5619cf0168b288edc0ab07
NemanjaSt97 <stojanovic.nemanja71@gmail.com>
2020-04-16 21:54:02 +0200

    Implemented registration process

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -9,14 +9,14 @@
                             <v-text-field
                                 label="First name"
                                 v-model="firstName"
-                                :rules="[rules.required, rules.charactersOnly, rules.nameLength]"
+                                :rules="[rules.required, rules.lettersOnly, rules.nameLength]"
                                 @input="validate"
                                 validate-on-blur
                             />
                             <v-text-field
                                 label="Last name"
                                 v-model="lastName"
-                                :rules="[rules.required, rules.charactersOnly, rules.nameLength]"
+                                :rules="[rules.required, rules.lettersOnly, rules.nameLength]"
                                 @input="validate"
                                 validate-on-blur
                             />
@@ -51,7 +51,7 @@
                                 validate-on-blur
                             />
                         </v-card-text>
-                        <v-card-actions><v-btn :disabled="!valid">Register</v-btn></v-card-actions>
+                        <v-card-actions><v-btn :disabled="!valid" @click="register()">Register</v-btn></v-card-actions>
                     </v-form>
                 </v-card>
             </v-col>
@@ -66,7 +66,7 @@
             rules() { return {
                 required: value => !!value || "Required.",
                 email: value => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value) || "Invalid e-mail.",
-                charactersOnly: v => ! /\d/.test(v) || 'Name contains just characters!',
+                lettersOnly: v => ! /\d/.test(v) || 'Name contains just characters!',
                 nameLength: v => v.length <= 20 || 'Name must be less than 20 characters!',
                 digitsOnly: v => /^\+?\d+$/.test(v) || 'Phone contains just digits!',
                 confirmPassword: v => v === this.password || 'Passwords must match!'
@@ -87,9 +87,15 @@
                              this.rules.required(this.firstName) === true && this.rules.required(this.lastName) === true &&
                              this.rules.required(this.confirmPassword) === true && this.rules.required(this.phone) === true &&
                              this.rules.email(this.email) === true && this.rules.confirmPassword(this.confirmPassword) === true &&
-                             this.rules.charactersOnly(this.firstName) === true && this.rules.charactersOnly(this.lastName) === true &&
+                             this.rules.lettersOnly(this.firstName) === true && this.rules.lettersOnly(this.lastName) === true &&
                              this.rules.nameLength(this.firstName) === true && this.rules.nameLength(this.lastName) === true &&
                              this.rules.digitsOnly(this.phone) === true
+            },
+            async register () {
+                await this.$store.dispatch('user/register', { email: this.email, password1: this.password,
+                                                                            password2: this.confirmPassword, first_name: this.firstName,
+                                                                            last_name: this.lastName, phone: this.phone })
+                this.$router.push({ name: 'User' })
             }
         }
     }
