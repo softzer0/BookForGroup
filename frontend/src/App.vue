caeeb9b714180fa37a5b32f24a7c54d441ce6e81
NemanjaSt97 <stojanovic.nemanja71@gmail.com>
2020-04-19 12:48:36 +0200

    Little syntax correction and cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,15 +6,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Header from "./components/Header";
 
 export default {
   name: 'App',
   components: {
     Header,
-    // eslint-disable-next-line vue/no-unused-components
-    HelloWorld
   }
 }
 </script>
