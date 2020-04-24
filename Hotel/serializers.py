fdbd67e5228b1d317636a54db93149ffbb4c6358
NemanjaSt97 <stojanovic.nemanja71@gmail.com>
2020-04-24 21:18:11 +0200

    [Backend] Creating a hotel by a logged in user

--- Hotel/serializers.py
+++ Hotel/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
-
+from authentication.models import User
 from authentication.serializers import UserDetailsSerializer
 from .models import Hotel
 
 
 class HotelSerializer(serializers.ModelSerializer):
-    user = UserDetailsSerializer(default=serializers.CurrentUserDefault())
+    user = UserDetailsSerializer(default=serializers.CurrentUserDefault(), read_only=True)
 
     class Meta:
         model = Hotel
         fields = ('user', 'name', 'city', 'address', 'web_site', 'image', 'free_parking', 'free_wifi', 'breakfast',
                   'swimming_pool', 'spa', 'gym')
+    
+    def create(self, validated_data):
+        return self.context['request'].user.hotels.create(**validated_data)
